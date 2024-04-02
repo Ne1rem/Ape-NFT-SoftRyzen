ed28fa431a489fd1362ea4b3d4256fcdaa694baf
Anatoliy Danilyuk <anatolydanilyuk2002@gmail.com>
2024-04-03 02:17:37 +0300

    MindMap part

--- src/components/MindMap/MindMap.jsx
+++ src/components/MindMap/MindMap.jsx
@@ -1,6 +1,7 @@
 import { useRef } from 'react';
 import css from './MindMap.module.css';
 import Slider from 'react-slick';
+import icon from '../../Image/symbol-defs.svg';
 
 const MindMap = () => {
   let sliderRef = useRef(null);
@@ -13,7 +14,7 @@ const MindMap = () => {
   const settings = {
     dots: false,
     infinite: false,
-    arrows:false,
+    arrows: false,
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
@@ -29,24 +30,41 @@ const MindMap = () => {
           }}
           {...settings}
         >
-          <div key={1}>
-            <h3>12</h3>
+          <div className={css.divSlaider} key={1}>
+            <p className={css.divSlaiderText}>
+              All owners of APE NFTs and all future collections will receive a
+              percentage of sales based on the number of NFTs they own
+            </p>
+            <p className={css.divSlaiderTitle}>YAPE DROP</p>
           </div>
-          <div key={2}>
-            <h3>223</h3>
+          <div className={css.divSlaider} key={2}>
+            <p className={css.divSlaiderText}>
+              Launch of the 2nd YACHT Collection Releasing the first version of
+              the Ape Slam Game
+            </p>
+            <p className={css.divSlaiderTitle}>New Collection</p>
           </div>
-          <div key={3}>
-            <h3>334</h3>
+          <div className={css.divSlaider} key={3}>
+            <p className={css.divSlaiderText}>
+              Launch your own token, the proceeds of which will go to a global
+              fund to LAUNCH YACHT CLUB AND PROMOTE it
+            </p>
+            <p className={css.divSlaiderTitle}>Token</p>
           </div>
-          <div key={4}>
-            <h3>445</h3>
+          <div className={css.divSlaiderFinal} key={4}>
+            <a href='/'>
+              <svg className={css.divSlaiderSvg}>
+                <use href={`${icon}#icon-arrow`} />
+              </svg>
+              <p className={css.divSlaiderTitle}>Learn more in mind map</p>
+            </a>
           </div>
         </Slider>
-        <div style={{ textAlign: 'center' }}>
-          <button className="button" onClick={previous}>
-            Previous
+        <div style={{ textAlign: 'center', paddingTop: '24px' }}>
+          <button className={css.buttonNextPrev} onClick={previous}>
+            Prev
           </button>
-          <button className="button" onClick={next}>
+          <button className={css.buttonNextPrev} onClick={next}>
             Next
           </button>
         </div>
