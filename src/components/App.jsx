be430d1aa6207d05137ec1910f33cc2cbf2bf28a
Anatoliy Danilyuk <anatolydanilyuk2002@gmail.com>
2024-04-02 13:12:54 +0300

    start project

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,25 @@
-export const App = () => {
+import About from './About/About';
+import Arts from './Arts/Arts';
+import ContactUs from './ContactUs/ContactUs';
+import FAQ from './FAQ/FAQ';
+import Footer from './Footer/Footer';
+import Header from './Header/Header';
+import Hero from './Hero/Hero';
+import MindMap from './MindMap/MindMap';
+
+const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homewor12321321k template
-    </div>
+    <>
+      <Header />
+      <Hero />
+      <About />
+      <MindMap />
+      <FAQ />
+      <Arts />
+      <ContactUs />
+      <Footer />
+    </>
   );
 };
+
+export default App;
